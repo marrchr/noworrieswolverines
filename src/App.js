a6479f6a3f994d8e672e25afae22c17815c0bd12
Chris Marr <marrchr@umich.edu>
2022-06-02 10:33:12 -0400

    First go at the circle slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import React, { useState, useRef } from "react";
+import { CircleSlider } from "react-circle-slider";
 import "./App.css";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const slider = useRef(null);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>No Worries Wolverines</h1>
+        <div style={{ position: "relative" }}>
+          <div className="textContainer">
+            {value}
+            <div className="minute">MINUTES</div>
+          </div>
+          <CircleSlider
+            ref={slider}
+            value={value}
+            stepSize={5}
+            onChange={(value) => setValue(value)}
+            size={250}
+            max={120}
+            gradientColorFrom="#ec008c"
+            gradientColorTo="#fc6767"
+            knobRadius={20}
+            circleWidth={20}
+          />
+        </div>
       </header>
     </div>
   );
